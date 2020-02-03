05c266d96d49798d65b163c8d061101450d0648c
Jeniamakarchik <eniamak@gmail.com>
2020-02-03 17:17:29 +0300

    add visual login link test

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,15 @@
-class BasePage:
-    def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
+from selenium.webdriver.common.by import By
 
-    def open_page(self):
-        self.browser.get(self.url)
+from .base_page import BasePage
+
+
+class MainPage(BasePage):
+    def __init__(self, browser, url):
+        super().__init__(browser, url)
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "There is no login link"
